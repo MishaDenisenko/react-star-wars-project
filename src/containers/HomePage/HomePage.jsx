2b265b2a98fc097edf914fd8497508b84a42ab91
mihailden <midenisenko@gmail.com>
2023-11-05 21:48:40 +0200

    add HomePage component

---
+++ src/containers/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from "./HomePage.module.css";
+
+
+const HomePage = props => {
+    return (
+        <>
+            <h1 className={"header__text"}>Home Page</h1>
+        </>
+    );
+};
+
+HomePage.propTypes = {};
+
+export default HomePage;
