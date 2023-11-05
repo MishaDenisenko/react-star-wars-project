1909e323aa7184180f8975157a3f1dba6c01ec6f
mihailden <midenisenko@gmail.com>
2023-11-05 17:02:48 +0200

    add ErrorMessage component

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -4,25 +4,31 @@ import {API_PEOPLE} from "../../constans/api";
 import {getApiResources} from "../../utils/network";
 import {getPeopleId, getPeopleImg} from "../../services/getPeopleData";
 import PeopleList from "../../components/PeoplePage/PeopleList";
+import {withErrorApi} from "../../hoc-helpers/withErrorApi";
 
-const PeoplePage = () => {
 
+const PeoplePage = ({setErrorApi}) => {
     const [people, setPeople] = useState([]);
+
+
     const getResource = async (url) => {
         const res = await getApiResources(url);
 
-        const peopleList = res.results.map(({name, url}) => {
-            const id = getPeopleId(url);
-            const img = getPeopleImg(id);
+        if (res) {
+            const peopleList = res.results.map(({name, url}) => {
+                const id = getPeopleId(url);
+                const img = getPeopleImg(id);
 
-            return {
-                id,
-                name,
-                img
-            }
-        });
+                return {
+                    id,
+                    name,
+                    img
+                }
+            });
 
-        setPeople(peopleList);
+            setPeople(peopleList);
+            setErrorApi(false);
+        } else setErrorApi(true);
     }
 
     useEffect(() => {
@@ -31,9 +37,10 @@ const PeoplePage = () => {
 
     return (
         <>
+            <h1>Navigation</h1>
             {people && <PeopleList people={people}/>}
         </>
     );
 };
 
-export default PeoplePage;
+export default withErrorApi(PeoplePage);
