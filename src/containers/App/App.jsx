b215cd04c4087030efc3bdce55a522a1fa743cf6
mihailden <midenisenko@gmail.com>
2023-11-05 22:16:23 +0200

    add basic routing

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,13 +1,24 @@
 import cn from "classnames";
-import styles from './App.module.css';
-
 import React from 'react';
-import PeoplePage from "../PeoplePage";
+import {Route, BrowserRouter, Routes} from "react-router-dom";
+
+import Header from "../../components/Header";
+
+import styles from './App.module.css';
+import routesConfig from "../../routes/routesConfig";
 
 const App = () => {
-  return (
-      <PeoplePage/>
-  );
+    return (
+        <BrowserRouter>
+            <Header/>
+
+            <Routes>
+                {routesConfig.map(({path, element}, index) => (
+                        <Route key={index} path={path} element={element}/>
+                    ))}
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
