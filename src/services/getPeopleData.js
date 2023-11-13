35131d4bcdc7a4d14653e752ce4f6fda7d2771a9
mihailden <midenisenko@gmail.com>
2023-11-13 22:59:05 +0200

    Updates

--- src/services/getPeopleData.js
+++
@@ -1,7 +0,0 @@
-import {URL_IMG_PERSON, GUIDE_IMG_EXTENSION} from "../constants/api";
-
-const getId = (url, category) => url.match(/people\/(\d*)/)[1]
-
-export const getPeoplePageId = (url) => Number(url.match(/page=(\d*)/)[1])
-export const getPeopleId = (url) => getId(url, 'people');
-export const getPeopleImg = id => URL_IMG_PERSON + '/' + id + GUIDE_IMG_EXTENSION;
